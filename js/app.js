063e9cd6e6bb6031380ad69a77f0200610139edb
Lindsay M <lminami23@punahou.edu>
2018-11-10 10:45:28 -1000

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -16,6 +16,28 @@ function generateScene(numRow) {
 		}
 		container.appendChild(row)
 	}
+	var pump = document.getElementsByClassName('pumpkin');
+	var randNum = Math.floor(Math.random() * pump.length)
+	//console.log(randNum)
+	var candy = document.createElement('img');
+	candy.src = "imgs/candy.jpg"
+	candy.setAttribute('class', 'candy');
+	pump[randNum].appendChild(candy)
+	//console.log(pump[randNum])
+
+	for (let i = 0; i < pump.length; i++) {
+		pump[i].addEventListener('mouseover', function(){
+			console.log(pump[i])
+			pump[i].style.backgroundImage = 'none'
+			if (pump[i] == pump[randNum]) {
+				console.log('yay')
+				alert('you found the candy. yay!')
+				setTimeout(function(){container.innerHTML = ''
+				generateScene(Math.floor(Math.random() * (7-3) + 3))
+			}, 1500)
+			}
+		})
+	}
 	
 }
 
